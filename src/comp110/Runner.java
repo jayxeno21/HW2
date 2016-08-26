447af45753002c025befe0274b1148a047ec2142
Kris Jordan <krisjordan@gmail.com>
2016-08-26 14:50:45 -0400

    Initial commit.

---
+++ src/comp110/Runner.java
@@ -0,0 +1,10 @@
+/*
+ * You do not need to modify anything in this file!
+ */
+package comp110;
+
+public class Runner extends Environment {
+  public static void main(String[] args) {
+    Environment.launch();
+  }
+}
---
+++ src/comp110/TrainerRoute.java
@@ -0,0 +1,21 @@
+package comp110;
+
+/**
+ * Author:
+ *
+ * ONYEN:
+ *
+ * Collaborator ONYEN(s):
+ *
+ * UNC Honor Pledge: I certify that no unauthorized assistance has been received
+ * or given in the completion of this work. I certify that I understand and
+ * could now rewrite on my own, without assistance from collaborators or course
+ * staff, the problem set code I am submitting.
+ */
+public class TrainerRoute {
+
+  void embark(Trainer changeYourTrainersNameHere) {
+
+  }
+
+}
