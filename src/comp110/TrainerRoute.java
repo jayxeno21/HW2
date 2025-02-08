3e7bf828b9ffc8de307a0b845e91515aee2b04f4
jaysen <114195312+jayxeno21@users.noreply.github.com>
2025-02-07 20:36:06 -0800

    hw2

--- src/comp110/TrainerRoute.java
+++ src/comp110/TrainerRoute.java
@@ -10,8 +10,37 @@ package comp110;
  */
 public class TrainerRoute {
 
-	void embark(Trainer changeYourTrainersNameHere) {
-
+	void embark(Trainer jaysen) {
+		jaysen.chooseGender("male");
+		jaysen.moveForward(2);
+		jaysen.moveForward(2);
+		jaysen.getPokemonFromOak("bulbasaur");
+		jaysen.moveBack(2);
+		jaysen.turnRight();
+		jaysen.moveForward(1);
+		jaysen.moveForward(1);
+		jaysen.turnLeft();
+		jaysen.moveForward(2);
+		jaysen.catchPokemon(4);
+		jaysen.turnLeft();
+		jaysen.moveForward(1);
+		jaysen.turnLeft();
+		jaysen.moveForward(4);
+		jaysen.moveBoulder();
+		jaysen.turnLeft();
+		jaysen.moveForward(2);
+		jaysen.getOnLapras();
+		jaysen.moveForward(4);
+		jaysen.turnRight();
+		jaysen.getOffLapras();
+		jaysen.pickupEgg();
+		jaysen.turnRight();
+		jaysen.moveForward(4);
+		jaysen.turnLeft();
+		jaysen.moveForward(2);
+		jaysen.turnRight();
+		jaysen.moveForward(4);
+		jaysen.useAttack();
 	}
 
 }
